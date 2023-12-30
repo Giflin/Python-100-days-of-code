3d189c8fb4bc71fe5072217ba33698b5d66aaad4
Giflin <114787368+Giflin@users.noreply.github.com>
2023-12-30 23:58:42 +0530

    Add files via upload

---
+++ Day 01.py
@@ -0,0 +1,23 @@
+print("Hello World!")
+s = int(input("Enter ur Age:"))
+print(f"I am {s} years old")
+print("Giflin \nGiflin")
+
+# identation error
+#  print("Hi")
+
+# Syntax error
+# print(("Hi")
+
+print("Hello " + input("What is your name: "))
+
+# variable naming
+#1variable is not good
+#variable name is not good
+
+
+print('Welcome to the Band Name Generator!')
+name=input("What is your city's name:\n")
+pets_name=input("What is your pet's name:\n")
+print("Your band name is "+name+" "+pets_name)
+
