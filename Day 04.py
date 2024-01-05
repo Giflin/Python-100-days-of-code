7e330d76326b68971da6be3b16abd8079bbe7798
Giflin <114787368+Giflin@users.noreply.github.com>
2024-01-05 12:00:31 +0530

    Day 04.py

--- Day 04.py
+++ Day 04.py
@@ -19,3 +19,26 @@ def answer(X, K):
 X = 9
 K = 5
 print(answer(X, K));
+
+
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen[1][1])
+
+#banker roulette
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input("Enter a number with letter of combination a,b,c and 1,2,3: ")
+letter=position[0].lower()
+abc=["a","b","c"]
+num=abc.index(letter)
+num1=int(position[1])
+map[num1-1][num]="X"
+print(f"{line1}\n{line2}\n{line3}")
